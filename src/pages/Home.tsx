985161af674ac91204f05d237d5fe474ccc2ce4b
Kyle Thornton <kyle@mavensconsulting.com>
2018-05-05 21:53:12 -0400

    add jersey to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,11 @@ interface State {
   content: {
     __html: string;
   };
+  jersey: string;
+}
+
+interface State {
+  jersey: string;
 }
 
 interface HomePage {
@@ -19,7 +24,8 @@ class Home extends React.Component<Props, State> {
   state = {
     content: {
       __html: ''
-    }
+    },
+    jersey: ''
   };
 
   getContent = async () => {
@@ -27,13 +33,22 @@ class Home extends React.Component<Props, State> {
     this.setState({ content: {__html: convertMarkdown(entry.fields.pageContent as string) }});
   }
 
+  getJersey = async () => {
+    const jersey = await client.getAsset('4tmTAsinv2AuquCWAi0iyW');
+    this.setState({ jersey: `${jersey.fields.file.url}?w=400` });
+  }
+
   componentDidMount() {
     this.getContent();
+    this.getJersey();
   }
 
   render() {
     return (
       <PageContent>
+        <div style={{ float: 'right', padding: 10, maxWidth: '100%' }}>
+          <img style={{ maxWidth: 300 }} src={this.state.jersey} />
+        </div>
         <div dangerouslySetInnerHTML={this.state.content} />
       </PageContent >
     );
