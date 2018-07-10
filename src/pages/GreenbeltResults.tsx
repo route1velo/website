72de5ebe59959b24232870135f877fb353ae88b1
Kyle Thornton <kyle@mavensconsulting.com>
2018-07-10 19:51:52 -0400

    add collapsibility for each race and standings within a series. collapse other series when another is opened

--- src/pages/GreenbeltResults.tsx
+++ src/pages/GreenbeltResults.tsx
@@ -36,6 +36,7 @@ interface State {
   series: {
     [key: string]: Series;
   };
+  openTables: Set<string>;
 }
 
 interface TableProps {
@@ -84,8 +85,26 @@ const ResultTable: React.SFC<TableProps> = ({ data, keyPrefix }) => {
 const SeriesWrapper = styled.div`
   border: 1px solid grey;
   border-radius: 5px;
-  padding: 5px;
-  margin-bottom: 5px;
+  padding: 5px 5px 5px 15px;
+  margin-bottom: 15px;
+  & h4 svg {
+    margin-right: 5px;
+  }
+`;
+
+const TableWrapper = styled.div`
+  border-top: 1px solid grey;
+  padding: 10px 5px;
+  margin-bottom: 15px;
+
+  &:first-child {
+    border: none;
+  }
+
+  & .media-heading {
+    font-size: 1.25rem;
+  }
+
 `;
 
 class SimplePage extends React.Component<Props, State> {
@@ -108,7 +127,8 @@ class SimplePage extends React.Component<Props, State> {
         displayed: false,
         collapsed: true,
       },
-    }
+    },
+    openTables: new Set(),
   };
 
   displayResults = async (sheet: ResultSheet) => {
@@ -155,8 +175,24 @@ class SimplePage extends React.Component<Props, State> {
 
   handleHeaderClick = (series: string) => {
     const newState = Object.assign({}, this.state);
+    // collapse everything else
+    Object.keys(newState.series).forEach( key => {
+      if (key !== series) {
+        newState.series[key].collapsed = false;
+      }
+    });
     newState.series[series].collapsed = !newState.series[series].collapsed;
     this.setState(newState);
+  }
+
+  handleTableCollapseClick = (tableName: string) => {
+    const openTables = Object.assign(new Set(), this.state.openTables);
+    if (this.state.openTables.has(tableName)) {
+      openTables.delete(tableName);
+    } else {
+      openTables.add(tableName);
+    }
+    this.setState({ openTables });
   }
 
   componentDidMount() {
@@ -180,7 +216,7 @@ class SimplePage extends React.Component<Props, State> {
             }
             return (
               <SeriesWrapper key={seriesName}>
-                <Media key={seriesName} className="mt-3">
+                <Media key={seriesName} className="mt-2">
                   <Media body={true}>
                     <Media heading={true} onClick={() => this.handleHeaderClick(seriesName)}>
                       {series.collapsed ? <FaChevronDown /> : <FaChevronRight />}
@@ -190,15 +226,19 @@ class SimplePage extends React.Component<Props, State> {
                       {
                         (seriesName === 'A Series' || seriesName === 'B Series') &&
                         series.data &&
-                        <Media>
-                          <Media body={true}>
-                            <Media heading={true}>
-                              Standings
+                        <TableWrapper>
+                          <Media className="mt-2">
+                            <Media body={true}>
+                              <Media heading={true} onClick={() => this.handleTableCollapseClick(seriesName + 'standings')}>
+                                {this.state.openTables.has(seriesName + 'standings') ? <FaChevronDown /> : <FaChevronRight />}
+                                Standings
                               </Media>
-                              <ResultTable keyPrefix={`${seriesName}Standings`} data={series.data.Standings} />
+                              <Collapse isOpen={this.state.openTables.has(seriesName + 'standings')}>
+                                <ResultTable keyPrefix={`${seriesName}Standings`} data={series.data.Standings} />
+                              </Collapse>
+                            </Media>
                           </Media>
-                        </Media>
-
+                        </TableWrapper>
                       }
 
                       {
@@ -208,14 +248,19 @@ class SimplePage extends React.Component<Props, State> {
                           .map( date => {
                             return (
                               series.data &&
-                              <Media key={`${seriesName}${date}`}>
-                                <Media body={true}>
-                                  <Media heading={true}>
-                                    {date}
+                              <TableWrapper>
+                                <Media className="mt-2">
+                                  <Media body={true}>
+                                    <Media heading={true} onClick={() => this.handleTableCollapseClick(seriesName + date)}>
+                                      {this.state.openTables.has(seriesName + date) ? <FaChevronDown /> : <FaChevronRight />}
+                                      {date}
+                                    </Media>
+                                    <Collapse isOpen={this.state.openTables.has(seriesName + date)}>
+                                      <ResultTable data={series.data[date]} key={date} keyPrefix={`${seriesName}`} />
+                                    </Collapse>
                                   </Media>
-                                  <ResultTable data={series.data[date]} key={date} keyPrefix={`${seriesName}`} />
                                 </Media>
-                              </Media>
+                              </TableWrapper>
                             );
                           })
                       }
